f64674c4726fd09ddb8d81a373f906c187006d37
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-05-15 23:21:45 +0200

    all-set

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-require("dotenv").config();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const urlparser = require("urlparser");
-const dns = require("dns");
-const {Schema} = mongoose;
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
-const app = express();
-
-// Connect to MongoDB using Mongoose
 mongoose.connect(process.env.DB_URL)
 
-// Define schema for URL collection
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   username: String,
 });
 const User = mongoose.model("User", UserSchema);
@@ -27,21 +18,16 @@ const ExerciseSchema = new Schema({
   duration: Number,
   date: Date,
 });
-
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/public", express.static(`${process.cwd()}/public`));
-
-// Root path handler
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 });
 
-app.get("/api/users", async(req, res) => {
+app.get("/api/users", async (req, res) => {
   const users = await User.find({}).select("_id username");
   if (!users) {
     res.send("No users");
@@ -50,28 +36,30 @@ app.get("/api/users", async(req, res) => {
   }
 })
 
-app.post("/api/users", async(req, res) => {
+app.post("/api/users", async (req, res) => {
   console.log(req.body)
   const userObj = new User({
     username: req.body.username
   })
 
-  try {
+  try{
     const user = await userObj.save()
     console.log(user);
     res.json(user)
-  } catch (error) {
-    console.log(error)
+  }catch(err){
+    console.log(err)
   }
+  
 })
 
 app.post("/api/users/:_id/exercises", async (req, res) => {
   const id = req.params._id;
   const { description, duration, date } = req.body
-  try {
+
+  try{
     const user = await User.findById(id)
-    if (!user) {
-      res.send("could not find user");
+    if (!user){
+      res.send("Could not find user")
     } else {
       const exerciseObj = new Exercise({
         user_id: user._id,
@@ -88,17 +76,18 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
         date: new Date(exercise.date).toDateString()
       })
     }
-  } catch(err) {
+  }catch(err){
     console.log(err);
-    res.send("There was an error saving exercise");
+    res.send("There was an error saving the exercise")
   }
 })
+
 
 app.get("/api/users/:_id/logs", async (req, res) => {
   const { from, to, limit } = req.query;
   const id = req.params._id;
   const user = await User.findById(id);
-  if(!user) {
+  if(!user){
     res.send("Could not find user")
     return;
   }
@@ -112,7 +101,7 @@ app.get("/api/users/:_id/logs", async (req, res) => {
   let filter = {
     user_id: id
   }
-  if(from || to) {
+  if(from || to){
     filter.date = dateObj;
   }
 
@@ -123,7 +112,7 @@ app.get("/api/users/:_id/logs", async (req, res) => {
     duration: e.duration,
     date: e.date.toDateString()
   }))
-
+  
   res.json({
     username: user.username,
     count: exercises.length,
@@ -132,6 +121,8 @@ app.get("/api/users/:_id/logs", async (req, res) => {
   })
 })
 
-app.listen(port, () => {
-  console.log("Your app is listening on port " + port);
-});
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
